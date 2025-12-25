1a801a12a214b4bba2372f65b240dd27a6cd65d2
Dixant <40229439+Dixant-Dev@users.noreply.github.com>
2025-12-25 23:51:37 +0530

    Error regarding types

--- types/employee.ts
+++ types/employee.ts
@@ -1,7 +1,11 @@
 export interface OrgTreeNode {
+  id?: number
   employee_id: number
   target: string
   pic: string | null
+  relationship_id?: string
+  effective_start_date?: string
+  effective_end_date?: string
   direct_reports: number
   indirect_reports: number
   children: OrgTreeNode[]
